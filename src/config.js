b623008b8f7db5701f3c926b9fde8217ad56a3a4
painchornds <2606249268@qq.com>
2024-08-27 17:57:07 +0800

    chore: init project

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export default {
-  host: 'https://k.hotaru.icu', // 后端ip ser.painchornds.cn
-  port: '443', // 端口
-  wsHost: 'wss://k.hotaru.icu'
+  host: '', // 后端ip
+  port: '', // 端口
+  wsHost: ''
 };
